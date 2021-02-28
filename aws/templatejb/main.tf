5fb23c93b5ccddb86bf57d3e67d7b7c033e7ea70
luis13byte <56192905+luis13byte@users.noreply.github.com>
2021-02-28 23:43:48 +0100

    Update main.tf

--- aws/templatejb/main.tf
+++ aws/templatejb/main.tf
@@ -1,8 +1,28 @@
 #
+# Preparar el uso de variables para datos sensibles (secrets)
+
+variable "my_access_key" {
+  description = "Access-key-for-AWS"
+  default = "no_access_key_value_found"
+}
+ 
+variable "my_secret_key" {
+  description = "Secret-key-for-AWS"
+  default = "no_secret_key_value_found"
+}
+ 
+output "access_key_is" {
+  value = var.my_access_key
+}
+ 
+output "secret_key_is" {
+  value = var.my_secret_key
+}
+
 # Preparar el directorio de trabajo para usarlo con AWS
 
 provider "aws" {
-	region = "eu-west-3"
-  access_key = "my_access_key" # proporcionar credenciales correspondientes
-	secret_key = "my_user_secret_key" 
+  region = "eu-west-3"
+  access_key = var.my_access_key
+  secret_key = var.my_user_secret_key
 }
