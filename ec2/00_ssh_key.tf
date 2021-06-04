cbae3a3527c30cb1f268c74bbb5c7dcb27f26694
luis13bit <lyupanqui@cifpfbmoll.eu>
2021-06-04 15:54:41 +0200

    Moving files

--- ec2/00_ssh_key.tf
+++
@@ -1,7 +0,0 @@
-#
-# Exportamos nuestra key SSH
-
-resource "aws_key_pair" "luis_back" {
-  key_name   = "luis_back"
-  public_key = file("id_rsa.pub")
-}
--- ec2/02_elastic_ip.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_eip" "elastic" {
-  instance = aws_instance.z30.id
-  vpc      = true
-  tags = {
-    Name = "${var.instance_name}-eip"
-  }
-}
