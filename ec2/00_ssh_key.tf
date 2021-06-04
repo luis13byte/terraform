820377201d98236b30e28734e10e856abef1edd8
Luis Acosta <56192905+luis13byte@users.noreply.github.com>
2021-06-04 15:46:03 +0200

    Change key pair name

--- ec2/00_ssh_key.tf
+++ ec2/00_ssh_key.tf
@@ -1,7 +1,7 @@
 #
 # Exportamos nuestra key SSH
 
-resource "aws_key_pair" "jb_back" {
-  key_name   = "jb_back"
+resource "aws_key_pair" "luis_back" {
+  key_name   = "luis_back"
   public_key = file("id_rsa.pub")
 }
